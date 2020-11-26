6a4a2ae3889c487f63548e3ffc1685b67965aec2
Kolja Windeler <Kolja.Windeler@gmail.com>
2020-11-26 21:54:07 +0100

    improved cookie parsing

--- custom_components/ytube_music_player/const.py
+++ custom_components/ytube_music_player/const.py
@@ -3,6 +3,7 @@ import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
 import logging
 import datetime
+import traceback
 from collections import OrderedDict
 from ytmusicapi import YTMusic
 
@@ -84,17 +85,19 @@ CONF_SELECT_SPEAKERS = 'select_speakers'
 DEFAULT_SELECT_SOURCE = DOMAIN + '_source'
 DEFAULT_SELECT_PLAYLIST = DOMAIN + '_playlist'
 DEFAULT_SELECT_SPEAKERS = DOMAIN + '_speakers'
-DEFAULT_HEADER_PATH = '/config/headers_auth.json'
+DEFAULT_HEADER_FILENAME = 'ytube_header.json'
 
 DEFAULT_SHUFFLE_MODE = 1
 DEFAULT_SHUFFLE = True
 
 ERROR_COOKIE = 'error_cookie'
+ERROR_AUTH_USER = 'error_auth_user'
+ERROR_GENERIC = 'error_generic'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend = vol.Schema({
 	DOMAIN: vol.Schema({
 		vol.Optional(CONF_RECEIVERS): cv.string,
-		vol.Optional(CONF_HEADER_PATH, default=DEFAULT_HEADER_PATH): cv.string,
+		vol.Optional(CONF_HEADER_PATH, default=DEFAULT_HEADER_FILENAME): cv.string,
 		vol.Optional(CONF_SELECT_SOURCE, default=DEFAULT_SELECT_SOURCE): cv.string,
 		vol.Optional(CONF_SELECT_PLAYLIST, default=DEFAULT_SELECT_PLAYLIST): cv.string,
 		vol.Optional(CONF_SELECT_SPEAKERS, default=DEFAULT_SELECT_SPEAKERS): cv.string,
@@ -105,36 +108,76 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend = vol.Schema({
 _LOGGER = logging.getLogger(__name__)
 
 
-def check_data(user_input):
+def check_data(user_input,path):
 	"""Check validity of the provided date."""
 	ret = {}
-	if(CONF_COOKIE in user_input) and (CONF_HEADER_PATH in user_input):
+	if(CONF_COOKIE in user_input):
 		try:
 			# sadly config flow will not allow to have a multiline text field
 			# we get a looong string that we've to rearrange into multiline for ytmusic
+
+			# the only thing we need is cookie + x-goog-authuser, lets try to cut this out
+			# so the fields are written like 'identifier':'value', but some values actually have ':' inside, bummer.
 			c = user_input[CONF_COOKIE]
-			c = c.replace('Cookie','\nCookie')
-			c = c.replace('Accept-Encoding','\nAccept-Encoding')
-			c = c.replace('Accept-Language','\nAccept-Language')
-			c = c.replace('Authorization','\nAuthorization')
-			c = c.replace('Host','\nHost')
-			c = c.replace('User-Agent','\nUser-Agent')
-			c = c.replace('X-Goog-AuthUser','\nX-Goog-AuthUser')
-			c = c.replace('X-Goog-Visitor-Id','\nX-Goog-Visitor-Id')
-			c = c.replace('x-origin','\nx-origin')
-			c = c.replace('X-YouTube-Client-Name','\nX-YouTube-Client-Name')
-			c = c.replace('X-Youtube-Identity-Token','\nX-Youtube-Identity-Token')
-			c = c.replace('X-YouTube-Page-CL','\nX-YouTube-Page-CL')
-			YTMusic.setup(filepath = user_input[CONF_HEADER_PATH], headers_raw = c)
+			clean_cookie = ""
+			clean_x_goog_authuser = ""
+			## lets try to find the cookie part
+			cookie_pos = c.lower().find('cookie')
+			if(cookie_pos>=0):
+				#_LOGGER.debug("found cookie in text")
+				cookie_end = c[cookie_pos:]
+				cookie_end_split = cookie_end.split(':')
+				if(len(cookie_end_split)>=3):
+					#_LOGGER.debug("found three or more sections")
+					cookie_length_last_field = cookie_end_split[2].rfind(' ')
+					if(cookie_length_last_field>=0):
+						#_LOGGER.debug("found a space")
+						cookie_length = len(cookie_end_split[0])+1+len(cookie_end_split[1])+1+cookie_length_last_field
+						clean_cookie = c[cookie_pos:cookie_pos+cookie_length]
+						#_LOGGER.debug(clean_cookie)
+			## lets try to find x-auth-part
+			xauth_pos = c.lower().find('x-goog-authuser: ')
+			if(xauth_pos>=0):
+				#_LOGGER.debug("found x-goog-authuser in text")
+				#_LOGGER.debug(c[xauth_pos+len('x-goog-authuser: '):])
+				xauth_len = c[xauth_pos+len('x-goog-authuser: '):].find(' ')
+				#_LOGGER.debug(xauth_len)
+				if(xauth_len>=0):
+					#_LOGGER.debug("found space in text")
+					clean_x_goog_authuser = c[xauth_pos:(xauth_pos+len('x-goog-authuser: ')+xauth_len)]
+					#_LOGGER.debug(clean_x_goog_authuser)
+			## lets see what we got
+			if(clean_cookie!="" and clean_x_goog_authuser!=""):
+				# woop woop, this COULD be it
+				c = clean_cookie+"\n"+clean_x_goog_authuser+"\n"
+			else:
+				# well we've failed to find the cookie, the only thing we can do is at least to help with some breaks
+				c = c.replace('cookie','\ncookie')
+				c = c.replace('Cookie','\nCookie')
+				c = c.replace('x-goog-authuser','\nx-goog-authuser')
+				c = c.replace('X-Goog-AuthUser','\nX-Goog-AuthUser')
+			#_LOGGER.debug("feeding with: ")
+			#_LOGGER.debug(c)
+			YTMusic.setup(filepath = path, headers_raw = c)
 			return {}
 		except Exception:
-			ret["base"] = ERROR_COOKIE
+			c = user_input[CONF_COOKIE].lower()
+			if(c.find('cookie') == -1 ):
+				_LOGGER.error("Field 'Cookie' not found!")
+				ret["base"] = ERROR_COOKIE
+			elif(c.find('X-Goog-AuthUser') == -1 ):
+				_LOGGER.error("Field 'X-Goog-AuthUser' not found!")
+				ret["base"] = ERROR_AUTH_USER
+			else:
+				_LOGGER.error("Generic setup error, please see below")
+				_LOGGER.error(traceback.format_exc())
+				ret["base"] = ERROR_GENERIC
 			return ret
 
-def ensure_config(user_input):
+def ensure_config(user_input,path):
 	"""Make sure that needed Parameter exist and are filled with default if not."""
 	out = {}
-	out[CONF_HEADER_PATH] = DEFAULT_HEADER_PATH
+	out[CONF_HEADER_PATH] = path
 #	out[CONF_ICON] = DEFAULT_ICON
 	out[CONF_RECEIVERS] = ''
 	out[CONF_SHUFFLE] = DEFAULT_SHUFFLE
@@ -161,13 +204,13 @@ def ensure_config(user_input):
 	return out
 
 
-def create_form(user_input):
+def create_form(user_input, path):
 	"""Create form for UI setup."""
-	user_input = ensure_config(user_input)
+	user_input = ensure_config(user_input,path)
 
 	data_schema = OrderedDict()
 	data_schema[vol.Required(CONF_COOKIE, default="")] = str
-	data_schema[vol.Required(CONF_HEADER_PATH, default=user_input[CONF_HEADER_PATH])] = str
+#	data_schema[vol.Required(CONF_HEADER_PATH, default=user_input[CONF_HEADER_PATH])] = str
 #	data_schema[vol.Required(CONF_RECEIVERS, default=user_input[CONF_RECEIVERS])] = str
 #	data_schema[vol.Optional(CONF_SHUFFLE, default=user_input[CONF_SHUFFLE])] = vol.Coerce(bool)
 #	data_schema[vol.Optional(CONF_SHUFFLE_MODE, default=user_input[CONF_SHUFFLE_MODE])] = vol.Coerce(int)
